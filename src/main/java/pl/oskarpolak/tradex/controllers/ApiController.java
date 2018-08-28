c9e3e2441a157dac05f7215ed02dd795160849b2
Oskix <oskar.polak@akademiakodu.pl>
2018-08-28 15:16:09 +0200

    firstCOmmit

--- src/main/java/pl/oskarpolak/tradex/controllers/ApiController.java
+++ src/main/java/pl/oskarpolak/tradex/controllers/ApiController.java
@@ -1,4 +1,30 @@
 package pl.oskarpolak.tradex.controllers;
 
+import com.sun.tools.javac.util.DefinedBy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import pl.oskarpolak.tradex.models.CurrencyModel;
+import pl.oskarpolak.tradex.models.services.CurrentService;
+
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+@RestController
 public class ApiController {
+
+    @Autowired
+    CurrentService currentService;
+
+    @GetMapping("/api/currency/{typed}")
+    public ResponseEntity getCurrency(@PathVariable("typed") String typed){
+         return ResponseEntity
+                 .ok()
+                 .body(currentService.getActualCurrencies()
+                        .stream()
+                        .filter(s -> s.contains(typed.toUpperCase()))
+                        .collect(Collectors.toList()));
+    }
 }
---
+++ src/main/java/pl/oskarpolak/tradex/models/services/CurrentService.java
@@ -0,0 +1,16 @@
+package pl.oskarpolak.tradex.models.services;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+import sun.jvm.hotspot.oops.Array;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class CurrentService {
+    @Bean
+    public List<String> getActualCurrencies(){
+         return Arrays.asList ("EUR", "USD", "CHF", "GBP", "AUD", "HKD");
+    }
+}
